3e8bbea83bae48c85c34d101a5665e5f9c362572
yfg1995 <andonovic995@gmail.com>
2024-08-27 14:45:55 +0200

    echart

--- src/components/Dashboard/DashboardWorkSummary.tsx
+++ src/components/Dashboard/DashboardWorkSummary.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import { ContainerBorderRounded } from "../ContainerBorderRounded";
+import { SmoothedLineChart } from "../SmoothedLineChart";
 
 export interface IDashboardWorkSummary {}
 
 export const DashboardWorkSummary: FC<IDashboardWorkSummary> = () => {
   return (
-    <ContainerBorderRounded className="max-w-[1080px]">
-      DashboardWorkSummary
+    <ContainerBorderRounded className="max-w-[1080px] h-[500px]">
+      <SmoothedLineChart />
     </ContainerBorderRounded>
   );
 };
--- src/components/SmoothedLineChart.tsx
+++ src/components/SmoothedLineChart.tsx
@@ -1,7 +1,65 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
+import * as echarts from "echarts";
 
 export interface ISmoothedLineChart {}
 
 export const SmoothedLineChart: FC<ISmoothedLineChart> = () => {
-  return <div>SmoothedLineChart</div>;
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      const chart = echarts.init(ref.current);
+
+      const option = {
+        xAxis: {
+          data: [
+            "Nov",
+            "Dec",
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "June",
+            "July",
+          ],
+          splitLine: {
+            show: true,
+            lineStyle: {
+              color: "#333",
+              width: 5,
+            },
+          },
+        },
+        yAxis: {
+          splitLine: {
+            show: false,
+          },
+        },
+        series: [
+          {
+            data: [150, 550, 600, 450, 700, 750, 650, 350, 500],
+            type: "line",
+            smooth: true,
+            lineStyle: {
+              color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                { offset: 0, color: "#c205e8" },
+                { offset: 1, color: "#620175" },
+              ]),
+              width: 3,
+            },
+            showSymbol: false,
+          },
+        ],
+      };
+
+      chart.setOption(option);
+
+      return () => {
+        chart.dispose();
+      };
+    }
+  }, []);
+
+  return <div className="h-full" ref={ref} />;
 };
